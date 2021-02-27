e95cdc8cf64407880ca248891a2cf86a35506e7e
Vincent <darkpaper28@gmail.com>
2021-02-26 20:51:32 -0500

    fin exercice 2

--- themes/theme-gv/header.php
+++ themes/theme-gv/header.php
@@ -44,7 +44,8 @@
 				<p class="site-description"><?php echo $theme_gv_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
-        
+        <?php
+		if ( is_front_page()) : ?>
 <section class="carrousel">
     <div>1</div>
     <div>2</div>
@@ -54,7 +55,7 @@
 <button id="un">1</button>
 <button id="deux">2</button>
 <button id="trois">3</button>
-
+<?php endif ?>
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="true"><img src="https://s2.svgbox.net/hero-outline.svg?ic=menu-alt-1&amp;color=000000" width="32" height="32"></button>

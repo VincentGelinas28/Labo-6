879572e835c15ed9aa10225219f745b62fa790ff
Vincent <darkpaper28@gmail.com>
2021-03-11 13:43:08 -0500

    fin de cours

--- themes/theme-gv/category-cours.php
+++ themes/theme-gv/category-cours.php
@@ -9,7 +9,7 @@
 
 get_header();
 ?>
-/////////////////////////////////archive
+//////////////////////////////// category-cours.php
 	<main id="primary" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
@@ -23,23 +23,27 @@ get_header();
 
 			<?php
 			/* Start the Loop */
+            $precedent = 0;
 			while ( have_posts() ) :
 				the_post();
+                $titre = get_the_title();
+                $session = substr($titre, 4,1);
+                $contenu = get_the_contenu();
+                $resume = substr($contenu,0 ,200);
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+                ?>
+                <?php
+                    if($session != $precedent){
+                        echo "<p>Session : " . $session ."<p>";
+                    }
+                    $precedent = $session;
+                ?>
+                <p> <?php the_title(); ?> <p>
+                <p> <?php the_content(); ?> <p>
 
+            <?php
 			endwhile;
 
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
 
 		endif;
 		?>

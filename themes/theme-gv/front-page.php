9758a27da097987f147933d3a1d66c0c5d090d71
Vincent <darkpaper28@gmail.com>
2021-04-09 20:46:09 -0400

    fin du labo 7

--- themes/theme-gv/front-page.php
+++ themes/theme-gv/front-page.php
@@ -10,6 +10,13 @@
 get_header();
 ?>
 
+<header class="page-header">
+				<?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header>
+
 <section class="carrousel">
 	
     <article class="slide_conteneur">
@@ -82,12 +89,7 @@ get_header();
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+			<!-- .page-header -->
 			<section class="cours">
 			<?php
 			/* Start the Loop */
@@ -144,3 +146,4 @@ function convertirTableu(&$tPropriété)
 
 
 }
+
